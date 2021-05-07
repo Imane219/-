fcafb576328489d001028119a3feb8a020bb1bf3
Unravel <1621690242@qq.com>
2021-05-07 22:54:13 +0800

    "v1.1"

--- internal/detectorpool/sfuzz.go
+++ internal/detectorpool/sfuzz.go
@@ -2,43 +2,43 @@ package detectorpool
 
 import (
 	"contrplatform/configs"
-	"contrplatform/pkg/line_reader"
+	"contrplatform/pkg/reader"
 	"io/ioutil"
 	"os"
 	"strings"
 )
 
 type SfuzzOutput struct {
-	Duration     string `json:"duration"`	//运行时长
-	Coverage     string `json:"coverage"`	//覆盖率
-	Branches     int    `json:"branches"`	//分支数
-	Predicates   int    `json:"predicates"`	//未覆盖分支数
-	Tracebits    int    `json:"tracebits"` //已覆盖分支数
+	Duration   string `json:"duration"`   //运行时长
+	Coverage   string `json:"coverage"`   //覆盖率
+	Branches   int    `json:"branches"`   //分支数
+	Predicates int    `json:"predicates"` //未覆盖分支数
+	Tracebits  int    `json:"tracebits"`  //已覆盖分支数
 }
 
 func (dp *DetectorPool) GetSfuzzOutputs(id string, outputs map[string]*Output) error {
-	tester,err := dp.detector(id)
+	tester, err := dp.detector(id)
 	if err != nil {
 		return err
 	}
-	if err=tester.checkOutputState();err!=nil{
+	if err = tester.checkOutputState(); err != nil {
 		return err
 	}
 
-	dirPath := configs.SfuzzOutputPath+"/"+id+"/"
-	filesInfo,err := ioutil.ReadDir(dirPath)
+	dirPath := configs.SfuzzOutputPath + "/" + id + "/"
+	filesInfo, err := ioutil.ReadDir(dirPath)
 	if err != nil {
 		return err
 	}
 
-	for _,fileInfo := range filesInfo {
+	for _, fileInfo := range filesInfo {
 		if fileInfo.IsDir() {
 			continue
 		}
-		if !strings.HasSuffix(fileInfo.Name(),".sol.txt"){
+		if !strings.HasSuffix(fileInfo.Name(), ".sol.txt") {
 			continue
 		}
-		file,err:=os.Open(dirPath+fileInfo.Name())
+		file, err := os.Open(dirPath + fileInfo.Name())
 		if err != nil {
 			return err
 		}
@@ -47,24 +47,24 @@ func (dp *DetectorPool) GetSfuzzOutputs(id string, outputs map[string]*Output) e
 		fileName := r.MustString()
 		contractName := r.MustString()
 		label := fileName + ":" + contractName
-		if _,ok:= outputs[label]; !ok {
-			outputs[label] = NewOutput(fileName,contractName)
+		if _, ok := outputs[label]; !ok {
+			outputs[label] = NewOutput(fileName, contractName)
 		}
 		outputSfuzz := outputs[label].Sfuzz
-		outputSfuzz.Duration=r.MustString()
-		outputSfuzz.Coverage=r.MustString()
-		outputSfuzz.Branches=r.MustInt()
-		outputSfuzz.Predicates=r.MustInt()
-		outputSfuzz.Tracebits=r.MustInt()
+		outputSfuzz.Duration = r.MustString()
+		outputSfuzz.Coverage = r.MustString()
+		outputSfuzz.Branches = r.MustInt()
+		outputSfuzz.Predicates = r.MustInt()
+		outputSfuzz.Tracebits = r.MustInt()
 		outputVul := outputs[label].Vul
 		outputVul.GaslessSend = r.MustBool()
 		outputVul.ExceptionDisorder = r.MustBool()
 		outputVul.Reentrancy = outputVul.Reentrancy || r.MustBool()
-		outputVul.TimeDependency = outputVul.TimeDependency ||r.MustBool()
+		outputVul.TimeDependency = outputVul.TimeDependency || r.MustBool()
 		outputVul.NumberDependency = r.MustBool()
 		outputVul.DelegateCall = r.MustBool()
 		outputVul.FreezingEther = r.MustBool()
-		outputVul.IntegerOverflow = outputVul.IntegerOverflow||r.MustBool()
+		outputVul.IntegerOverflow = outputVul.IntegerOverflow || r.MustBool()
 		outputVul.IntegerUnderflow = outputVul.IntegerUnderflow || r.MustBool()
 		_ = file.Close()
 	}
--- pkg/reader/reader.go
+++ pkg/reader/reader.go
@@ -18,20 +18,20 @@ func New(rd io.Reader) *LineReader {
 }
 
 func (lr *LineReader) MustString() string {
-	str,err:= lr.b.ReadString('\n')
-	if err!=nil{
+	str, err := lr.b.ReadString('\n')
+	if err != nil {
 		return ""
 	}
-	return strings.TrimSuffix(str,"\n")
+	return strings.TrimSuffix(str, "\n")
 }
 
 func (lr *LineReader) MustInt() int {
-	str,err:=lr.b.ReadString('\n')
-	if err != nil{
+	str, err := lr.b.ReadString('\n')
+	if err != nil {
 		return 0
 	}
-	str = strings.TrimSuffix(str,"\n")
-	n,err:= strconv.Atoi(str)
+	str = strings.TrimSuffix(str, "\n")
+	n, err := strconv.Atoi(str)
 	if err != nil {
 		return 0
 	}
@@ -39,14 +39,14 @@ func (lr *LineReader) MustInt() int {
 }
 
 func (lr *LineReader) MustBool() bool {
-	str,err:=lr.b.ReadString('\n')
+	str, err := lr.b.ReadString('\n')
 	if err != nil {
 		return false
 	}
-	str = strings.TrimSuffix(str,"\n")
-	n,err:=strconv.Atoi(str)
-	if err!=nil{
+	str = strings.TrimSuffix(str, "\n")
+	n, err := strconv.Atoi(str)
+	if err != nil {
 		return false
 	}
-	return n!=0
+	return n != 0
 }
